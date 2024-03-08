edcbe64ce988e17faa0a0dbe7033fc8e18d37998
Warda Kamran <wkamran523@gmail.com>
2024-03-08 09:52:51 -0800

    14.ASSIGNMENT

---
+++ main.js
@@ -0,0 +1,4 @@
+var guest_list = ['Warda', 'Waleed', 'Aqsa', 'Fathma'];
+for (var i = 0; i < guest_list.length; i++) {
+    console.log('Respected Sir/Madum' + guest_list[i] + ',\n\nWe invited you on dinner tomorrow,\n\nThank You');
+}
---
+++ main.ts
@@ -0,0 +1,4 @@
+let guest_list : string [] = ['Warda','Waleed','Aqsa','Fathma'];
+for(let i=0; i<guest_list.length; i++){
+    console.log('Respected Sir/Madum' + guest_list[i] + ',\nWe invited you on dinner tomorrow,\n\nThank You')
+}
